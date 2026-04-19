4f09a87b535e0f67079996a016f7cb971cf2a90b
carbuyermate <carbuyermate@gmail.com>
2026-04-19 16:52:46 +0100

    feat: add Google Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/navbar";
@@ -20,6 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2TQ444EGZN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2TQ444EGZN');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${outfit.variable}`}>
         <ThemeProvider
           attribute="data-theme"
