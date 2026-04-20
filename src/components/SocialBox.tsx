19d6a0f8f5bcddb1a1619767a73bb534994716d3
carbuyermate <carbuyermate@gmail.com>
2026-04-20 11:21:19 +0100

    style: rename X / Twitter to X and update YT link

--- src/components/SocialBox.tsx
+++ src/components/SocialBox.tsx
@@ -27,10 +27,10 @@ export function SocialBox() {
                     <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
                         <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.259 5.631 5.905-5.631zm-1.161 17.52h1.833L7.084 4.126H5.117L17.083 19.77z"/>
                     </svg>
-                    <span>X / Twitter</span>
+                    <span>X</span>
                 </a>
                 <a
-                    href="https://www.youtube.com/@RAPwUK"
+                    href="https://www.youtube.com/@rapwuktv"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="social-box__link social-box__link--yt"
