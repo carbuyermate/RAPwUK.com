fdb22e4a076fa198614c8e447ec76321c2c891e9
carbuyermate <carbuyermate@gmail.com>
2026-04-29 12:22:06 +0100

    fix: remove encrypted-media from spotify iframe to stop DRM prompt

--- src/app/rappers/[slug]/page.tsx
+++ src/app/rappers/[slug]/page.tsx
@@ -33,12 +33,12 @@ const renderSpotifyEmbed = (url?: string) => {
             return (
                 <div className="spotify-embed-container">
                     <iframe
-                        style={{ borderRadius: '12px' }}
+                        style={{ borderRadius: '12px', maxWidth: '100%' }}
                         src={`https://open.spotify.com/embed/${type}/${id}?utm_source=generator&theme=0`}
                         width="100%"
                         height="152"
                         frameBorder="0"
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        allow="autoplay; clipboard-write; fullscreen; picture-in-picture"
                         loading="lazy"
                     ></iframe>
                 </div>
